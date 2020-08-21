b056bb335c4b4d893ca180e140e9fd54c72e0f6d
Dilip Sundarraj <dilip.sundarraj@target.com>
2020-08-21 07:03:38 -0500

    fixed the compilation issues

--- src/main/java/com/learnjava/util/CommonUtil.java
+++ src/main/java/com/learnjava/util/CommonUtil.java
@@ -2,20 +2,12 @@ package com.learnjava.util;
 
 import org.apache.commons.lang3.time.StopWatch;
 
+import static com.learnjava.util.LoggerUtil.log;
 import static java.lang.Thread.sleep;
 
 public class CommonUtil {
 
     public static StopWatch stopWatch = new StopWatch();
-
-   public static void startTimer(){
-       stopWatch.start();
-   }
-
-    public static long timeTaken(){
-        stopWatch.stop();
-        return stopWatch.getTime();
-    }
 
     public static void delay(long delayMilliSeconds)  {
         try{
@@ -26,5 +18,25 @@ public class CommonUtil {
 
     }
 
+    public static String transForm(String s) {
+        CommonUtil.delay(500);
+        return s.toUpperCase();
+    }
 
+    public static void startTimer(){
+        stopWatch.start();
+    }
+
+    public static void timeTaken(){
+        stopWatch.stop();
+        log("Total Time Taken : " +stopWatch.getTime());
+    }
+
+    public static void stopWatchReset(){
+        stopWatch.reset();
+    }
+
+    public static  int noOfCores(){
+        return Runtime.getRuntime().availableProcessors();
+    }
 }
