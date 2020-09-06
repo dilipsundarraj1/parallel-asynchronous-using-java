923ec8a8e2ab49e07838cb97fd416c82372fdabb
Dilip Sundarraj <dilip.sundarraj@target.com>
2020-09-06 04:38:03 -0500

    added some tests in checkoutservice

--- src/main/java/com/learnjava/service/PriceValidatorService.java
+++ src/main/java/com/learnjava/service/PriceValidatorService.java
@@ -4,11 +4,13 @@ package com.learnjava.service;
 import com.learnjava.domain.checkout.CartItem;
 
 import static com.learnjava.util.CommonUtil.delay;
+import static com.learnjava.util.LoggerUtil.log;
 
 public class PriceValidatorService {
 
     public boolean isCartItemInvalid(CartItem cartItem){
         int cartId = cartItem.getItemId();
+        log("isCartItemInvalid : "+ cartItem);
         delay(500);
         if (cartId == 7 || cartId == 9 || cartId == 11) {
             return true;
--- src/test/java/com/learnjava/service/CheckoutServiceTest.java
+++ src/test/java/com/learnjava/service/CheckoutServiceTest.java
@@ -6,6 +6,8 @@ import com.learnjava.domain.checkout.CheckoutStatus;
 import com.learnjava.util.DataSet;
 import org.junit.jupiter.api.Test;
 
+import java.util.concurrent.ForkJoinPool;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class CheckoutServiceTest {
@@ -14,6 +16,16 @@ class CheckoutServiceTest {
 
     CheckoutService checkoutService = new CheckoutService(priceValidatorService);
 
+    @Test
+    void parallelism() {
+        // parallelism =  no of cores -1
+        System.out.println("parallelism : " +ForkJoinPool.getCommonPoolParallelism());
+    }
+
+    @Test
+    void no_of_cores() {
+        System.out.println("no of cores : " +Runtime.getRuntime().availableProcessors());
+    }
 
     @Test
     void checkout_6_items() {
@@ -36,6 +48,22 @@ class CheckoutServiceTest {
         //  System.setProperty("java.util.concurrent.ForkJoinPool.common.parallelism", "100");
 
         Cart cart = DataSet.createCart(12);
+        System.out.println("size of the cart  : "+ cart.getCartItemList().size());
+        //when
+        CheckoutResponse checkoutResponse = checkoutService.checkout(cart);
+
+        //then
+        assertEquals(CheckoutStatus.FAILURE, checkoutResponse.getCheckoutStatus());
+    }
+
+    @Test
+    void checkout_modify_parallelism() {
+
+        //given
+        // -Djava.util.concurrent.ForkJoinPool.common.parallelism=100
+          System.setProperty("java.util.concurrent.ForkJoinPool.common.parallelism", "100");
+
+        Cart cart = DataSet.createCart(105);
 
         //when
         CheckoutResponse checkoutResponse = checkoutService.checkout(cart);
