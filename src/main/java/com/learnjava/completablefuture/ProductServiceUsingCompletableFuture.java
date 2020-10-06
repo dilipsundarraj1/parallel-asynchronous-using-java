d617d54367385fc7b7c4c737dedcf4342c0d5eaa
Dilip Sundarraj <dilip.sundarraj@target.com>
2020-10-05 20:43:33 -0500

    added the partial working code

---
+++ src/main/java/com/learnjava/completablefuture/ProductServiceUsingCompletableFuture.java
@@ -0,0 +1,131 @@
+package com.learnjava.completablefuture;
+
+import com.learnjava.domain.*;
+import com.learnjava.service.InventoryService;
+import com.learnjava.service.ProductInfoService;
+import com.learnjava.service.ReviewService;
+
+import java.util.List;
+import java.util.concurrent.CompletableFuture;
+import java.util.stream.Collectors;
+
+import static com.learnjava.util.CommonUtil.startTimer;
+import static com.learnjava.util.CommonUtil.timeTaken;
+import static com.learnjava.util.LoggerUtil.log;
+
+public class ProductServiceUsingCompletableFuture {
+    private ProductInfoService productInfoService;
+    private ReviewService reviewService;
+    private InventoryService inventoryService;
+
+    public ProductServiceUsingCompletableFuture(ProductInfoService productInfoService, ReviewService reviewService) {
+        this.productInfoService = productInfoService;
+        this.reviewService = reviewService;
+    }
+
+    public ProductServiceUsingCompletableFuture(ProductInfoService productInfoService, ReviewService reviewService, InventoryService inventoryService) {
+        this.productInfoService = productInfoService;
+        this.reviewService = reviewService;
+        this.inventoryService = inventoryService;
+    }
+
+    public Product retrieveProductDetails(String productId) {
+
+        startTimer();
+        CompletableFuture<ProductInfo> cfProductInfo = CompletableFuture.supplyAsync(() -> productInfoService.retrieveProductInfo(productId));
+        CompletableFuture<Review> cfReview = CompletableFuture.supplyAsync(() -> reviewService.retrieveReviews(productId));
+
+        Product product = cfProductInfo
+                .thenCombine(cfReview, (productInfo, review) -> new Product(productId, productInfo, review))
+                .join(); // blocks the thread
+        timeTaken();
+        return product;
+    }
+
+    public CompletableFuture<Product> retrieveProductDetails_CF(String productId) {
+
+        CompletableFuture<ProductInfo> cfProductInfo = CompletableFuture.supplyAsync(() -> productInfoService.retrieveProductInfo(productId));
+        CompletableFuture<Review> cfReview = CompletableFuture.supplyAsync(() -> reviewService.retrieveReviews(productId));
+
+        return cfProductInfo
+                .thenCombine(cfReview, (productInfo, review) -> new Product(productId, productInfo, review));
+    }
+
+
+    public Product retrieveProductDetailsWithInventory(String productId) {
+
+        startTimer();
+        CompletableFuture<ProductInfo> cfProductInfo = CompletableFuture.supplyAsync(() -> productInfoService.retrieveProductInfo(productId))
+                .thenApply((productInfo -> {
+                    productInfo.setProductOptions(updateInventoryToProductOption(productInfo));
+                    return productInfo;
+                }));
+
+        CompletableFuture<Review> cfReview = CompletableFuture.supplyAsync(() -> reviewService.retrieveReviews(productId));
+
+        Product product = cfProductInfo
+                .thenCombine(cfReview, (productInfo, review) -> new Product(productId, productInfo, review))
+                .join(); // blocks the thread
+        timeTaken();
+        return product;
+    }
+
+    public Product retrieveProductDetailsWithInventory_approach2(String productId) {
+
+        startTimer();
+        CompletableFuture<ProductInfo> cfProductInfo = CompletableFuture.supplyAsync(() -> productInfoService.retrieveProductInfo(productId))
+                .thenApply((productInfo -> {
+                    productInfo.setProductOptions(updateInventoryToProductOption_CF(productInfo));
+                    return productInfo;
+                }));
+
+        CompletableFuture<Review> cfReview = CompletableFuture.supplyAsync(() -> reviewService.retrieveReviews(productId));
+
+        Product product = cfProductInfo
+                .thenCombine(cfReview, (productInfo, review) -> new Product(productId, productInfo, review))
+                .join(); // blocks the thread
+        timeTaken();
+        return product;
+    }
+
+
+    private  List<ProductOption> updateInventoryToProductOption(ProductInfo productInfo){
+
+        List<ProductOption> productOptionList = productInfo.getProductOptions()
+                .stream()
+                .map(productOption -> {
+                    Inventory inventory = inventoryService.addInventory(productOption);
+                    productOption.setInventory(inventory);
+                    return productOption;
+                })
+                .collect(Collectors.toList());
+
+        return productOptionList;
+    }
+
+    private  List<ProductOption> updateInventoryToProductOption_CF(ProductInfo productInfo){
+
+        List<CompletableFuture<ProductOption>> productOptionList = productInfo.getProductOptions()
+                .stream()
+                .map(productOption -> inventoryService.addInventory_CF(productOption)
+                         .thenApply((inventory -> {
+                             productOption.setInventory(inventory);
+                             return productOption;
+                         })))
+                .collect(Collectors.toList());
+
+       return productOptionList.stream().map(CompletableFuture::join)
+               .collect(Collectors.toList());
+    }
+
+    public static void main(String[] args) {
+
+        ProductInfoService productInfoService = new ProductInfoService();
+        ReviewService reviewService = new ReviewService();
+        ProductServiceUsingCompletableFuture productService = new ProductServiceUsingCompletableFuture(productInfoService, reviewService);
+        String productId = "ABC123";
+        Product product = productService.retrieveProductDetails(productId);
+        log("Product is " + product);
+
+    }
+}
