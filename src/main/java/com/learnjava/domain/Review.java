cd48a6b7678cac31123197f5f4d2fb28fccbff39
dilipsundarraj <dilip.bandlasundarraj@gmail.com>
2020-10-17 10:12:00 -0500

    added the builder

--- src/main/java/com/learnjava/domain/Review.java
+++ src/main/java/com/learnjava/domain/Review.java
@@ -1,12 +1,14 @@
 package com.learnjava.domain;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @AllArgsConstructor
 @NoArgsConstructor
 @Data
+@Builder
 public class Review {
     private int noOfReviews;
     private double overallRating;
