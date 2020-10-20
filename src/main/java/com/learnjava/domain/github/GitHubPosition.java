9691742d3d80435564a10604ee36c9495a976261
Dilip Sundarraj <dilip.sundarraj@target.com>
2020-10-19 20:55:53 -0500

    added the githubposition

---
+++ src/main/java/com/learnjava/domain/github/GitHubPosition.java
@@ -0,0 +1,26 @@
+package com.learnjava.domain.github;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class GitHubPosition {
+    private String id;
+    private String type;
+    private String url;
+    private String created_at;
+    private String company;
+    private String company_url;
+    private String location;
+    private String title;
+    private String description;
+    private String how_to_apply;
+    private String company_logo;
+
+}
