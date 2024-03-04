02540e4c7b9f92726412573ed4592ffa12fd2aab
dilip sundarraj <dilip.bandlasundarraj@gmail.com>
2024-03-04 05:03:08 -0600

    resolved conflicts

--- src/main/java/com/learnjava/apiclient/MoviesClient.java
+++ src/main/java/com/learnjava/apiclient/MoviesClient.java
@@ -18,107 +18,73 @@ public class MoviesClient {
         this.webClient = webClient;
     }
 
-    public Movie retrieveMovie(Long movieInfoId) {
+    public Movie retrieveMovie(Long movieInfoId){
+        //movieInfo
         var movieInfo = invokeMovieInfoService(movieInfoId);
+        //review
         var reviews = invokeReviewsService(movieInfoId);
         return new Movie(movieInfo, reviews);
     }
 
-    public List<Movie> retrieveMovieList(List<Long> movieInfoIds) {
+    public List<Movie> retrieveMovies(List<Long> movieInfoIds){
 
         return movieInfoIds
                 .stream()
-                //.parallel()
                 .map(this::retrieveMovie)
                 .collect(Collectors.toList());
-
     }
 
-    public CompletableFuture<Movie> retrieveMovie_CF(Long movieInfoId) {
-
-        var movieInfo = CompletableFuture.supplyAsync(() -> invokeMovieInfoService(movieInfoId));
-        var reviews = CompletableFuture.supplyAsync(() -> invokeReviewsService(movieInfoId));
-        return movieInfo.thenCombine(reviews, Movie::new);
-    }
-
-    public List<Movie> retrieveMovieList_CF(List<Long> movieInfoIds) {
+    public List<Movie> retrieveMovieList_CF(List<Long> movieInfoIds){
 
         var movieFutures = movieInfoIds
                 .stream()
-                .parallel()
                 .map(this::retrieveMovie_CF)
                 .collect(Collectors.toList());
-
 
         return movieFutures
                 .stream()
                 .map(CompletableFuture::join)
                 .collect(Collectors.toList());
-
     }
 
-    public List<Movie> retrieveMovieList_CF_allOf(List<Long> movieInfoIds) {
-
-        var movieFutures = movieInfoIds
-                .stream()
-                .parallel()
-                .map(this::retrieveMovie_CF)
-                .collect(Collectors.toList());
-
-        var cfAllOf = CompletableFuture.allOf(movieFutures.toArray(new CompletableFuture[movieFutures.size()]));
-
-         return cfAllOf
-                .thenApply(v-> movieFutures.stream()
-                            .map(CompletableFuture::join)
-                            .collect(Collectors.toList()))
-                 .join();
-    }
-
-    public List<Movie> retrieveMovieList_CF_anyOf(List<Long> movieInfoIds) {
-
-        var movieFutures = movieInfoIds
-                .stream()
-                .parallel()
-                .map(this::retrieveMovie_CF)
-                .collect(Collectors.toList());
-
-        var cfAllOf = CompletableFuture.allOf(movieFutures.toArray(new CompletableFuture[movieFutures.size()]));
-
-        return cfAllOf
-                .thenApply(v-> movieFutures.stream()
-                        .map(CompletableFuture::join)
-                        .collect(Collectors.toList()))
-                .join();
+    public CompletableFuture<Movie> retrieveMovie_CF(Long movieInfoId){
+        var movieInfo = CompletableFuture.supplyAsync(()->invokeMovieInfoService(movieInfoId));
+        //review
+        var reviews =CompletableFuture.supplyAsync(()-> invokeReviewsService(movieInfoId));
+        return movieInfo
+                .thenCombine(reviews, Movie::new);
     }
 
     private List<Review> invokeReviewsService(Long movieInfoId) {
 
-        String uri = UriComponentsBuilder.fromUriString("/v1/reviews")
+        ///v1/reviews?movieInfoId=1
+        var reviewUri = UriComponentsBuilder.fromUriString("/v1/reviews")
                 .queryParam("movieInfoId", movieInfoId)
                 .buildAndExpand()
-                .toUriString();
+                .toString();
 
-        List<Review> reviews = webClient.get().uri(uri)
+
+        return webClient
+                .get()
+                .uri(reviewUri)
                 .retrieve()
                 .bodyToFlux(Review.class)
                 .collectList()
                 .block();
 
-        return reviews;
     }
 
+    private MovieInfo invokeMovieInfoService(Long movieInfoId) {
 
-    public MovieInfo invokeMovieInfoService(Long movieInfoId) {
+        var moviesInfoUrlPath = "/v1/movie_infos/{movieInfoId}";
 
-        var movieInfoUrlPath = "/v1/movie_infos/{movieInfoId}";
-
-        var movieInfo = webClient.get()
-                .uri(movieInfoUrlPath, movieInfoId)
+        return webClient
+                .get()
+                .uri(moviesInfoUrlPath, movieInfoId)
                 .retrieve()
                 .bodyToMono(MovieInfo.class)
                 .block();
 
-        return movieInfo;
 
     }
 }
--- src/test/java/com/learnjava/apiclient/MoviesClientTest.java
+++ src/test/java/com/learnjava/apiclient/MoviesClientTest.java
@@ -1,130 +1,86 @@
 package com.learnjava.apiclient;
 
+import com.learnjava.apiclient.MoviesClient;
 import com.learnjava.util.CommonUtil;
-import org.junit.jupiter.api.*;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.RepeatedTest;
 import org.springframework.web.reactive.function.client.WebClient;
 
 import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @Disabled
 class MoviesClientTest {
 
-    WebClient webClient = WebClient.builder().baseUrl("http://localhost:8080/movies").build();
+
+    WebClient webClient = WebClient.builder()
+            .baseUrl("http://localhost:8080/movies")
+            .build();
+
     MoviesClient moviesClient = new MoviesClient(webClient);
 
-
-    @BeforeEach
-    void setUpMoviesClient() {
-        var movieInfoId = 1L;
-        moviesClient.retrieveMovie(movieInfoId);
-    }
-
-    @Test
     @RepeatedTest(10)
     void retrieveMovie() {
         CommonUtil.startTimer();
         //given
         var movieInfoId = 1L;
-
         //when
-
         var movie = moviesClient.retrieveMovie(movieInfoId);
 
-
+        System.out.println("movie : " + movie);
+        CommonUtil.timeTaken();
         //then
         assert movie!=null;
         assertEquals("Batman Begins", movie.getMovieInfo().getName());
         assert movie.getReviewList().size() == 1;
-
-        CommonUtil.timeTaken();
     }
 
-    @Test
     @RepeatedTest(10)
-    void retrieveMovie_1() {
+    void retrieveMovies() {
         CommonUtil.startTimer();
         //given
-        var movieInfoId = 1L;
-
+        var movieInfoIds = List.of(1L,2L, 3L,4L,5L,6L,7L);
         //when
+        var movies = moviesClient.retrieveMovies(movieInfoIds);
 
-        var movie = moviesClient.retrieveMovie(movieInfoId);
-
-
-        //then
-        assert movie!=null;
-        assertEquals("Batman Begins", movie.getMovieInfo().getName());
-        assert movie.getReviewList().size() == 1;
-
+        System.out.println("movie : " + movies);
         CommonUtil.timeTaken();
+        //then
+        assert movies!=null;
+        assert movies.size()==7;
     }
 
-    @Test
     @RepeatedTest(10)
-    void retrieveMovie_CF() {
-        //given
-        var movieInfoId = 1L;
-
-        //when
-        CommonUtil.startTimer();
-        var movie = moviesClient.retrieveMovie_CF(movieInfoId).join();
-        CommonUtil.timeTaken();
-        //then
-        assert movie!=null;
-        assertEquals("Batman Begins", movie.getMovieInfo().getName());
-        assert movie.getReviewList().size() == 1;
-
-
-    }
-
-
-    @Test
-    void retrieveMovieList() {
-        //given
-        var movieInfoIds = List.of(1L,2L, 3L , 4L, 5L, 6L, 7L);
-
-        //when
-        CommonUtil.startTimer();
-        var movies = moviesClient.retrieveMovieList(movieInfoIds);
-        System.out.println("movies : " + movies);
-        CommonUtil.timeTaken();
-
-        //then
-        assert movies.size() == 7;
-    }
-
-
-    @Test
     void retrieveMovieList_CF() {
-        //given
-        var movieInfoIds = List.of(1L,2L, 3L , 4L, 5L, 6L, 7L);
-
-        //when
         CommonUtil.startTimer();
+        //given
+        var movieInfoIds = List.of(1L,2L, 3L,4L,5L,6L,7L);
+        //when
         var movies = moviesClient.retrieveMovieList_CF(movieInfoIds);
 
-        System.out.println("movies : " + movies);
+        System.out.println("movie : " + movies);
         CommonUtil.timeTaken();
         //then
-        assert movies.size() == 7;
-
+        assert movies!=null;
+        assert movies.size()==7;
     }
 
-    @Test
-    void retrieveMovieList_CF_approach2() {
-        //given
-        var movieInfoIds = List.of(1L,2L, 3L , 4L, 5L, 6L, 7L);
 
-        //when
+    @RepeatedTest(10)
+    void retrieveMovie_CF() {
         CommonUtil.startTimer();
-        var movies = moviesClient.retrieveMovieList_CF_allOf(movieInfoIds);
+        //given
+        var movieInfoId = 1L;
+        //when
+        var movie = moviesClient.retrieveMovie_CF(movieInfoId)
+                .join();
 
-        System.out.println("movies : " + movies);
+        System.out.println("movie : " + movie);
         CommonUtil.timeTaken();
         //then
-        assert movies.size() == 7;
-
+        assert movie!=null;
+        assertEquals("Batman Begins", movie.getMovieInfo().getName());
+        assert movie.getReviewList().size() == 1;
     }
 }
